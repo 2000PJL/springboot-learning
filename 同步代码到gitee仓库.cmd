9a6fd372582ec5f22d65307fed36b14ad02b4c71
2000PJL <1985343015@qq.com>
2022-01-03 20:23:37 +0800

    考前测试

--- 同步代码到gitee仓库.cmd
+++ 同步代码到gitee仓库.cmd
@@ -3,7 +3,7 @@ git add .
 set /p var="Please into commit message: "
 @echo git commit -m "bat auto push:%date:~0,10%,%time:~0,8%" 
 @git commit -m  "%VAR%"
-git push springboot_gitee master
+git push -f springboot_gitee master
 @echo 已经完成
 
 pause
